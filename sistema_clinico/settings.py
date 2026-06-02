dfd73fdcacca9ad252bd1b6be7a64d3f35815a3d
Steven Rivas <rs22042@ues.edu.sv>
2026-06-01 19:25:03 -0600

    agregar manejo de correo

--- sistema_clinico/settings.py
+++ sistema_clinico/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,6 +25,19 @@ LOGIN_URL = 'usuarios:login'
 #Cuando el usuario cierra sesión lo redirige al login
 LOGOUT_REDIRECT_URL = 'usuarios:login'
 
+#MANEJO DE CORREOS ELECTRONICOS
+load_dotenv()
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
